213932a926e0deaf802b94a3cec494673bc3b5fd
Mawuli-Kplim Asase <masase@c1r16s7.42lausanne.ch>
2026-05-10 17:26:39 +0200

    working on wordpress

---
+++ srcs/requirements/wordpress/tools/init.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+sleep 10
+
+wp config create --allow-root \ 
+                --dbname=$SQL_DATABASE \
+                --dbuser=$SQL_USER \
+                --dbpass=$SQL_PASSWORD \
+                --dbhost=mariadb:3306 \
+                --path='/var/www/wordpress'
+
+wp core install --allow-root \
+                --url=$DOMAIN_NAME \ 
+                
+
+wp user create 
