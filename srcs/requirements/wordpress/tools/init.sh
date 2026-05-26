8b4029e8cde7a565c0ee0917234fd7a7df644b55
bubblemaw <mawuli.asase@gmail.com>
2026-05-26 14:22:31 +0000

    small addition

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -10,7 +10,21 @@ wp config create --allow-root \
                 --path='/var/www/wordpress'
 
 wp core install --allow-root \
-                --url=$DOMAIN_NAME \ 
+                --url=$DOMAIN_NAME \
+                --title=$SITE_TITLE \
+                --admin_user=$ADMIN_USER \
+                --admin_password=$ADMIN_PASSWORD \
+                --admin_email=$ADMIN_EMAIL \
+                --path=/var/www/wordpress
                 
 
-wp user create 
+wp user create --allow-root \
+            $WP_USER \
+            $WP_USER_EMAIL \
+            --role=author \
+            --user-pass=$WP_USER_PASSWORD \
+            --path=/var/www/wordpress
+
+mkdir -p /run/php
+
+exec php-fpm
